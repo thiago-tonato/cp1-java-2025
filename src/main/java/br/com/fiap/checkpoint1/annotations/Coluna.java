e0b2a3262524643e3ebb85fa3d95672b413f908f
thiago-tonato <tonato.thiago@gmail.com>
2025-03-18 20:51:16 -0300

    adiconado annotations Coluna e Tabela

---
+++ src/main/java/br/com/fiap/checkpoint1/annotations/Coluna.java
@@ -0,0 +1,13 @@
+package br.com.fiap.checkpoint1.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Coluna {
+    String nome();
+    boolean chavePrimaria() default false;
+}
