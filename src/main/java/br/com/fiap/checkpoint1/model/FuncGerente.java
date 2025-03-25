1bcbbf33590dabd6e45ffc4d5770f3aa13617030
garcia2202 <tonato.thiago@gmail.com>
2025-03-24 23:59:33 -0300

    modificada a classe FuncGerente para simplificar importações e corrigir a anotação da coluna de bônus.

--- src/main/java/br/com/fiap/checkpoint1/model/FuncGerente.java
+++ src/main/java/br/com/fiap/checkpoint1/model/FuncGerente.java
@@ -1,17 +1,12 @@
 package br.com.fiap.checkpoint1.model;
 
-import br.com.fiap.checkpoint1.annotations.Coluna;
-import br.com.fiap.checkpoint1.annotations.Tabela;
-import org.hibernate.annotations.Entity;
+import javax.persistence.*;
 
-import javax.persistence.DiscriminatorValue;
-
-@Tabela(nome = "TAB_FUNCIONARIO")
 @Entity
 @DiscriminatorValue("Gerente")
 public class FuncGerente extends Funcionario {
 
-    @Coluna(nome = "bonus_gerente")
+    @Column(name = "bonus_gerente")
     private static final double BONUS_GERENTE = 500.0;
 
     public FuncGerente(String nome, int horasTrabalhadas, double valorPorHora) {
