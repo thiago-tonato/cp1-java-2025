1c8ff7cf66fcb5636b058eb1a48c20fe779f1138
thiago-tonato <tonato.thiago@gmail.com>
2025-03-25 20:47:50 -0300

    Adicionadas documentações Javadoc para os métodos CRUD na classe FuncionarioDAO, detalhando operações de persistência e parâmetros utilizados.

--- src/main/java/br/com/fiap/checkpoint1/EntityManager/FuncionarioDAO.java
+++ src/main/java/br/com/fiap/checkpoint1/EntityManager/FuncionarioDAO.java
@@ -6,10 +6,22 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * Classe DAO (Data Access Object) responsável por gerenciar as operações
+ * de persistência relacionadas à entidade Funcionario.
+ * Utiliza JPA (Java Persistence API) para interação com o banco de dados.
+ */
 public class FuncionarioDAO {
 
+    // Cria uma fábrica de EntityManager usando a unidade de persistência configurada
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("CLIENTE_ORACLE");
 
+    /**
+     * Salva um novo funcionário no banco de dados.
+     * Realiza uma operação de INSERT.
+     *
+     * @param funcionario O funcionário a ser salvo
+     */
     public static void salvar(Funcionario funcionario) {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
@@ -18,6 +30,13 @@ public class FuncionarioDAO {
         em.close();
     }
 
+    /**
+     * Busca um funcionário pelo seu ID no banco de dados.
+     * Realiza uma operação de SELECT.
+     *
+     * @param id O ID do funcionário a ser buscado
+     * @return O funcionário encontrado ou null se não existir
+     */
     public static Funcionario buscar(Long id) {
         EntityManager em = emf.createEntityManager();
         Funcionario funcionario = em.find(Funcionario.class, id);
@@ -25,6 +44,12 @@ public class FuncionarioDAO {
         return funcionario;
     }
 
+    /**
+     * Atualiza os dados de um funcionário existente no banco de dados.
+     * Realiza uma operação de UPDATE.
+     *
+     * @param funcionario O funcionário com os dados atualizados
+     */
     public static void atualizar(Funcionario funcionario) {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
@@ -33,6 +58,12 @@ public class FuncionarioDAO {
         em.close();
     }
 
+    /**
+     * Remove um funcionário do banco de dados pelo seu ID.
+     * Realiza uma operação de DELETE.
+     *
+     * @param id O ID do funcionário a ser removido
+     */
     public static void deletar(Long id) {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
